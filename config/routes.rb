0e34b209b772359f3265c4a0b68bb07743119781
“YuriShozen” <155799910+w2yuri@users.noreply.github.com>
2024-02-16 08:03:18 +0000

    [更新]14章まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :books, only: [:new, :create, :index, :show, :destroy]
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to:"homes#top"
   get 'homes/about' => 'homes#about', as: :about
-end
+  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  end
